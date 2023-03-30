63add783e41c09517a3da54f921839363b681342
carlos abaffy <abaffy@gmail.com>
2023-03-30 15:45:47 -0400

    Final version assignmet 3

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -1,57 +1,87 @@
 # Importing required libraries
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 import time
 
 # Setting up the webdriver
 driver = webdriver.Chrome()
 
-# # Navigating to the Amazon.ca homepage
-driver.get("https://www.amazon.ca")
+#
+# # Navigating to the Ebay.ca homepage
+driver.get("https://www.ebay.ca")
 time.sleep(3)
+
 #
 # # Finding the search bar and entering text
-# # search_bar = driver.find_element_by_id("id","twotabsearchtextbox")
-search_bar = driver.find_element("id", "twotabsearchtextbox")
-search_bar.send_keys("laptop")
+search_bar = driver.find_element("id", "gh-ac")
+search_bar.send_keys("ip camera outdoor")
+
 #
 # # Submitting the search query
 search_bar.send_keys(Keys.RETURN)
+
 #
 # # Waiting for the search results page to load
 time.sleep(5)
+
 #
 # # Verifying that the search results page has loaded
-assert "laptop" in driver.title
+assert "ip camera outdoor" in driver.title
+
 #
-# # Selecting a laptop from the search results
-# # laptop_link = driver.find_element_by_css_selector("span[data-component-type='s-product-image'] a")
-laptop_link = driver.find_element("xpath", "/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[1]/div/div/div[1]/div[2]/div[2]/div[1]/div/a[2]/div/div[1]/img")
-# # laptop_link = driver.find_element("By.CSS_SELECTOR","span[data-component-type='s-product-image'] a")
-laptop_link.click()
-# /html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[1]/div/div/div[1]/div[2]/div[2]/div[1]/div/a[2]/div/div[1]/img
+# # Selecting a camera from the search results
+camera_link = driver.find_element("xpath", "/html/body/div[8]/div[4]/div[2]/div[1]/div[2]/ul/li[2]/div/div[2]/a/div/span")
+camera_link.click()
+
 #
-# # Waiting for the laptop details page to load
+# # Waiting for the ip camera details page to load
 time.sleep(5)
+
 #
-# # Verifying that the correct laptop details page has loaded
-# # assert "laptop" in driver.title
+# # The camera details opens in a new window
+# # store the original handle and switch to the new window
+home_window = driver.window_handles
+product_window = driver.window_handles[1]
+driver.switch_to.window(product_window)
+
 #
-# # Adding the laptop to the cart
-add_to_cart_button = driver.find_element("id","add-to-cart-button")
+# # Verifying that the correct camera details page has loaded
+assert "4K 8MP Turret Indoor/Outdoor Network PoE IP Camera Sony Image Sensor H.265" in driver.title
+
+#
+# # Adding the camera to the cart
+add_to_cart_button = driver.find_element("link text", "Add to cart")
 add_to_cart_button.click()
+
 #
 # # Waiting for the cart to update
 time.sleep(5)
+
 #
-# # Clicking on no thanks button
-no_thanks_button= driver.find_element("xpath","/html/body/div[5]/div[3]/div[1]/div/div/div[2]/div[2]/div/div/div[3]/div/span[2]/span/input")
-no_thanks_button.click()
-time.sleep(2)
-#
-# # Verifying that the laptop has been added to the cart
-cart_count = driver.find_element("id","nav-cart-count")
+# # Verifying that the ip camera  has been added to the cart
+cart_count = driver.find_element("xpath","/html/body/div[4]/header/div[1]/ul[2]/li[6]/div/a")
 assert cart_count.text == "1"
+
 #
-# # Closing the webdriver
-driver.close()
+# # Changing the quantity to 3 cameras
+quantity_select = Select(driver.find_element("xpath", "/html/body/div[5]/div[2]/div/div[3]/div[1]/div/ul/li/div/div/div/div[1]/div/div[3]/div/div[1]/div[1]/div/span/span/select"))
+quantity_select.select_by_value('3')
+
+#
+# # Waiting for the cart to update
+time.sleep(5)
+
+#
+# # Verifying that the cart have been updated to 3 cameras
+cart_count = driver.find_element("xpath","/html/body/div[4]/header/div[1]/ul[2]/li[6]/div/a")
+assert cart_count.text == "3"
+
+#
+# # Check the language of the page is English
+current_language = driver.find_element("partial link text", "Current language")
+assert current_language.text == "English"
+
+#
+# # Closing all the windows of the webdriver
+driver.quit()
